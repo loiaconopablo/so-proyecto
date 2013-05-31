c62b2a953e66d0e1760b2665ef8752ea7ff3d640
loiaconopablo@gmail.com <loiaconopablo@gmail.com@bcd96e49-1d5b-298c-d80e-45954f7b1c6b>
2013-05-31 00:12:11 +0000

    Modificaciones en el Block y Memory

--- SistemaOperativo/src/so-proyecto/Memory.py
+++ SistemaOperativo/src/so-proyecto/Memory.py
@@ -6,9 +6,14 @@ Created on 27/05/2013
 class Memory:
     
     def __init__(self, aMemPolicity):
-        self.physicalMemory={1024}
-        self.blocks = []
-        self.freeBlocks = []
+        self.physicalMemory={x: None for x in range(1,1025)} #Devuelve un dictionary con 1024 celdas vacias
+        #=======================================================================
+        # a= {x: None for x in range(1,1025)}
+        # print (a)  
+        #         El resultado que da es: {1: None, 2: None, 3: None,.... 1023: None, 1024: None}
+        #=======================================================================
+        self.blocks = [] #memoria logica
+        self.freeBlocks = [] #memoria logica
         self.policityMemory= aMemPolicity()
     
     def sizeMemory(self):
@@ -25,15 +30,18 @@ class Memory:
             if self.isFreeRoomTo(aPCB):
                 self.assignBlock(aPCB) 
             else:
-                self.compact()
-                self.assignBlock(aPCB)
+                self.compact(aPCB.getSize()) #Se le dice Compacta y se le pasa el tamaño que se necesista, asi compacta 
+                self.assignBlock(aPCB)       #hasta llegar al tamaño que se necesita y para
         else:
             longScheduler.push(aPCb)
             
     def assignBlock(self, aPCB):
-        blockUse = self.aMemPolicity.savePCB(self.freeBlocks, aPCB) #usa el bloque libre segun politica, lo saca de la lista de free y asigna pcb
-        self.addInstruction(blockUse.getDirBase(), aPCB.getInstruction())
+        #agregado Logico
+        blockUse = self.aMemPolicity.savePCB(self.freeBlocks, aPCB) #extrae bloque libre segun politica, lo saca de la lista de free y asigna pcb     
         self.addBlock(blockUse) #agrega el bloque "usado" con el pcb a la lista de bloques
+        
+        #agregado Fisico
+        self.addInstruction(blockUse.getDirBase(), aPCB.getInstruction())
         
     def addInstruction(self, aDirBase, aListInstruction):
         dirBaseMem = aDirBase
@@ -44,7 +52,12 @@ class Memory:
     def isFreeRoomTo(self, aPCB):
         return self.policityMemory.isFreeRoomTo(self.freeBlocks, aPCB)
     
-    
+    def giveMeFreeBlockFromCell(self, aDirCell):
+        for block in self.freeBlocks:
+            if block.containDir(aDirCell):
+                return (self.freeBlocks.pop(block))
+                break
+        
     #def compact(self):
         
     def releaseMemory(self, baseCell, endCell):
