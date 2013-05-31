4d292ae84339c9e9a119c539de6d44ce278c6c4c
loiaconopablo@gmail.com <loiaconopablo@gmail.com@bcd96e49-1d5b-298c-d80e-45954f7b1c6b>
2013-05-31 00:12:11 +0000

    Modificaciones en el Block y Memory

--- trunk/SistemaOperativo/src/so-proyecto/Block.py
+++ trunk/SistemaOperativo/src/so-proyecto/Block.py
@@ -21,11 +21,31 @@ class Block:
 
     def setDirBase(self,direction):
         self.dirBase=direction
-
+        
+    def getDirEnd(self):
+        return self.dirEnd
+        
     def cellDestino(self):
-        return self.dirFin+1
+        if not self.lastCell():
+            return (self.dirFin+1)
+        else: 
+            return None
+    
+    def cellAnterior(self):
+        if not self.firstCell():
+            return (self.dirBase-1)
+        else: 
+            return None
         
-        
+    def lastCell(self):
+        return (self.dirBase==0)
+    
+    def firstCell(self):
+        return (self.dirEnd==1024)
+             
+    def containDir(self, aDirCell): #pregunta si una direccion esta incluida en el bloque
+       # return (aDirCell in range(self.getDirBase(),self.getDirEnd()+1)) Otra Opcion de hacerlo
+        return (self.getDirBase()>= aDirCell and self.getDirEnd()<= aDirCell)
     
     #===========================================================================
     # def getInMemory(self):
