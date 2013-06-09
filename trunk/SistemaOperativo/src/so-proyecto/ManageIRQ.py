acfebed6748fbcf93538970bf1c0c71b2ac50dac
loiaconopablo@gmail.com <loiaconopablo@gmail.com@bcd96e49-1d5b-298c-d80e-45954f7b1c6b>
2013-06-09 23:31:26 +0000

    Se agrego una mem de policity la de Mejor ajuste, se modifico otras cosas.

--- trunk/SistemaOperativo/src/so-proyecto/ManageIRQ.py
+++ trunk/SistemaOperativo/src/so-proyecto/ManageIRQ.py
@@ -7,17 +7,18 @@ from Kernel import *
 
 class ManageIRQ:
     def __init__(self, aKernel):
-            self.kernel= aKernel
+            self.kernel = aKernel
         
     def iOInterrupt(self, pcb):
         self.kernel.modoOff()
-        print("Se mando un proceso a la cola de I/O")
         self.kernel.handlerIO.handle(pcb)
+        print("Se mando un proceso a la cola de I/O")
         self.kernel.modoOn()
 
     def killInterrupt(self, pcb):
         self.kernel.modoOff()
-        self.kernel.cpu.pcb=null # borrar el pcb terminado, ver si lo guardamos en alguna lista
+        self.kernel.pcbFinish.append(self.kernel.cpu.getPCB())  # se guarda el pcb finalizado en la lista.
+        self.kernel.cpu.pcb = null  # borrar el pcb terminado
         print("Finalizo el proceso actual")
         self.kernel.contextSwitch()
         self.kernel.modoOn()
@@ -34,7 +35,7 @@ class ManageIRQ:
         self.handler.nilInterrupt()    
         self.kernel.modoOn()
         
-    def newInterrupt(self,process):
+    def newInterrupt(self, process):
         print("Entro un nuevo proceso")
         self.kernel.insertProcess(process)
         self.kernel.reschedule()    
